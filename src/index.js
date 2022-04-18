467ab0fef566f1aaf7f80a66c602c1ca857af9b8
Daniel Sainero <dsainero@paradigmadigital.com>
2022-04-18 11:12:11 +0200

    add router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './views/home'
+import PodcastDetail from './views/podcast-detail'
+import PodcastChapter from './views/podcast-chapter'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route
+          exact
+          path='/podcast/:podcastId'
+          element={<PodcastDetail />}
+        ></Route>
+        <Route
+          exact
+          path='/podcast/:podcastId/episode/:episodeId'
+          element={<PodcastChapter />}
+        ></Route>
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
-);
-
+)
