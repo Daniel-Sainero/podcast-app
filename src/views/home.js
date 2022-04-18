e6659347c134d1cdbc7ed4e13204d71a3f5f32b7
Daniel Sainero <dsainero@paradigmadigital.com>
2022-04-18 11:53:11 +0200

    add axios and API hook

--- src/views/home.js
+++ src/views/home.js
@@ -1,3 +1,11 @@
-const Home = (props) => <div>Home view</div>
+import useFetch from '../apiService/useFetch'
+
+const Home = (props) => {
+  const { data, error } = useFetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json');
+  if(error){
+    console.log(error)
+ };
+  return (data && <div> {data.map(item => <div key={item.id.attributes['im:id']}>{item["im:artist"].label}</div>)}</div>)
+}
 
 export default Home
